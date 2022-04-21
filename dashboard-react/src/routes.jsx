72ff3022954b49ec4994650baf726cb26ce047b3
BeatrizNFerreira255 <beatriz.ferreira@bandtec.com.br>
2022-04-20 21:14:25 -0300

    Atualizações

--- dashboard-react/src/routes.jsx
+++ dashboard-react/src/routes.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/DashGenerico";
 import Login from "./pages/Login";
+import Salas from "./componentes/Salas";
 
 function Rotas() {
     return (
@@ -9,6 +10,7 @@ function Rotas() {
             <Routes>
                 <Route path="/" exact element={<Login />} />
                 <Route path="/home" exact element={<Home />} />
+                <Route path="/salas" exact element={<Salas />} />
 
             </Routes>
         </BrowserRouter>
