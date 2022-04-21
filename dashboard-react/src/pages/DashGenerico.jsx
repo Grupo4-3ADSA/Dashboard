b906f46640e548f9c65b77cfff761e8f70e8c24a
Guilherme da Silva Fonseca <guilherme.fonseca@berghem.com.br>
2022-04-20 21:25:23 -0300

    função menu

--- dashboard-react/src/pages/DashGenerico.jsx
+++ dashboard-react/src/pages/DashGenerico.jsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 import '../html-css-template/css/style.css';
 import LogoOnclnBranco from '../html-css-template/imagens/logo-branco.png';
 import IconAvatar from '../html-css-template/imagens/usuario.png';
 import IconConfig from '../html-css-template/imagens/configuracao.png';
 import IconSino from '../html-css-template/imagens/sino.png';
 import Home from '../componentes/HomeDash'
-import {Link} from 'react-router-dom'
+import Salas from '../componentes/Salas'
 
 function ItemNavbar(props) {
+
+    const [conteudo, setConteudo] = useState([<Home />]);
+
+    const data = [{ "name": ["Home", "Cadastrar/Editar", "Salas", "Painel de Controle", "Consumo/Equipamento", "Relatorio de Fatura", "Resumo de Consumo", "Central de Ajuda"] }]
+    const listItems = data.map((d) => <ul>{d.name.map((a) => <li key={a} onClick={() => clicado(a)}>{a}</li>)} </ul>)
+
+    function clicado(element) {
+        switch (element) {
+            case "Home":
+                console.log("FOI O HOME")
+                setConteudo(<Home />)
+                break;
+
+            case "Cadastrar/Editar":
+                console.log("FOI O CADASTRAR")
+
+                break;
+
+            case "Salas":
+                console.log("FOI O salas")
+                setConteudo(<Salas />)
+                break;
+
+            case "Painel de Controle":
+                console.log("FOI O PAINEL")
+                break;
+
+            case "Consumo/Equipamento":
+                console.log("FOI O CONSUMO")
+                break;
+
+            case "Relatorio de Fatura":
+                console.log("FOI O RELATORIO")
+                break;
+
+            case "Resumo de Consumo":
+                console.log("FOI O RESUMO")
+                break;
+
+            case "Central de Ajuda":
+                console.log("FOI O AJUDA")
+                break;
+
+            default:
+                break;
+        }
+    }
+
     return (
         <>
             <div clas="container">
@@ -30,18 +78,11 @@ function ItemNavbar(props) {
                     <div class="container-dash">
                         <div class="nav-esquerda">
                             <ul>
-                                <li>Home</li>
-                                <li>Cadastrar/Editar</li>
-                                <li> Salas</li>
-                                <li>Painel de controle</li>
-                                <li>Consumo/equipamento</li>
-                                <li>Relatório de fatura</li>
-                                <li>Resumo de consumo</li>
-                                <li>Central de ajuda</li>
+                                {listItems}
                             </ul>
                         </div>
                         <div class="conteudo">
-                        <Home/>
+                            {conteudo}
                         </div>
                     </div>
                 </div>
