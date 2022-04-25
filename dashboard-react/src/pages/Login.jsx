c9bf4b45b69e07148bc3b909368f12989c7fc8f0
elizeu12 <elizeu.souza@bandtec.com.br>
2022-04-25 17:32:40 -0300

    login validation ok

--- dashboard-react/src/pages/Login.jsx
+++ dashboard-react/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import '../html-css-template/css/style.css';
 import logoOncln from '../html-css-template/imagens/logo-cln.png'
 import fundoLogin from '../html-css-template/imagens/fundo-login.png'
@@ -11,16 +11,14 @@ function Login(props) {
     const [login, setLogin] = useState([])
     const [senha, setSenha] = useState([])
 
-    function autenticacao(login, senha) {
-
-        navigate("/dash-generico")
-
+    function autenticacao(e) {
+        e.preventDefault();
         autentica.ApiLogin.post("/autenticacao", {
             login: login,
             senha: senha
         })
             .then(response => {
-                if (response.status == 200) {
+                if (response.status === 200) {
                     console.log("Login feito com sucesso")
                     navigate("/dash-generico")
                 } else{
@@ -55,7 +53,7 @@ function Login(props) {
                             </div>
                             <div class="formAcess">
                                 <label for="name" class="input-fild senha">
-                                    <input type="text" id="nomeEmpresa" name="senha" defaultValue={senha}
+                                    <input type="password" id="nomeEmpresa" name="senha" defaultValue={senha}
                                         placeholder="Senha:" required minlength="4" onChange={e => setSenha(e.target.value)} />
                                     <div class="underline"></div>
                                 </label>
